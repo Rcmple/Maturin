d055b741114b8c998980067ea1df2c5e6efdbd57
Rcmple <imaginelipa@ImagineLipa>
2024-07-30 15:25:58 +0300

    trying again

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 #include <tgbot/tgbot.h>
+
 int main() {
-    std::cout << "Hello world" << '\n';
-    std::cout << 3 + 5 << '\n';
+    TgBot::Bot bot("7260576332:AAE7UfGaMTTf_wj6Pi4Ax1OCYUZYDV60h6c");
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "Hi!");
+    });
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+        printf("User wrote %s\n", message->text.c_str());
+        if (StringTools::startsWith(message->text, "/start")) {
+            return;
+        }
+        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+    });
+    try {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        TgBot::TgLongPoll longPoll(bot);
+        while (true) {
+            printf("Long poll started\n");
+            longPoll.start();
+        }
+    } catch (TgBot::TgException& e) {
+        printf("error: %s\n", e.what());
+    }
     return 0;
 }
